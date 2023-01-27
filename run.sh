f4a19ccc9a7309cb8705f55da5b26d2766ba12e0
Senquan Lu <senquan.lu@cern.ch>
2023-01-27 09:33:19 +0100

    All in one

---
+++ run.sh
@@ -0,0 +1,39 @@
+set -e
+
+speaker=llg
+stage=5
+dir=dataset_tmp/$speaker
+mkdir -p $dir
+
+if [ ${stage} == 1 ]; then
+  python autoslice.py --idir $dir/source --odir $dir/cut || exit -1
+  pushd $dir/cut
+  find . | grep 'wav' | nl -nrz -w3 -v1 | while read n f; do mv "$f" "${speaker}_$n.wav"; done || exit -1
+  popd
+fi
+
+if [ ${stage} == 2 ]; then
+  export TF_FORCE_GPU_ALLOW_GROWTH=true
+  spleeter separate -o $dir/clean -p spleeter:2stems $dir/cut/*.wav || exit -1
+fi
+if [ ${stage} == 3 ]; then
+  pushd $dir/clean 
+  echo * | tr ' ' '\n' | while read f; do mv $f/vocals.wav $f.wav; done || exit -1
+  mkdir -p ../clean2
+  mv *.wav ../clean2/
+  rm -rf ./*
+  popd
+fi
+if [ ${stage} == 4 ]; then
+  python autoslice2.py --ag 3 --in_path $dir/clean2 || exit -1
+fi
+if [ ${stage} == 5 ]; then
+  mkdir -p dataset_raw/$speaker/
+  mv $dir/split/*.wav dataset_raw/$speaker/
+fi
+
+python resample.py
+
+python preprocess_flist_config.py
+
+python preprocess_hubert_f0.py
