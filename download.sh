648e3987061057b102fe5ed558170dde743b2679
ame192 <whitestar1992@gmail.com>
2023-01-27 16:26:30 +0100

    download the models

---
+++ download.sh
@@ -0,0 +1,7 @@
+# 一键下载
+# hubert
+wget -P hubert/ https://github.com/bshall/hubert/releases/download/v0.1/hubert-soft-0d54a1f4.pt
+# G与D预训练模型
+wget -P logs/32k/ https://huggingface.co/innnky/sovits_pretrained/resolve/main/G_0.pth
+wget -P logs/32k/ https://huggingface.co/innnky/sovits_pretrained/resolve/main/D_0.pth
+
